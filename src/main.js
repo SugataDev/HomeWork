5a16b918a38a2aa0154d67cbd7e7edc358b2b3a9
SugataDev <sugata.chakma20@gmail.com>
2026-07-17 00:36:22 +0600

    FAQ items updated

--- src/main.js
+++ src/main.js
@@ -1,27 +1,19 @@
-// document.querySelectorAll(".group.cursor-pointer").forEach((card) => {
-//   card.addEventListener("click", () => {
-//     const link = card.querySelector(".project-link");
-//     if (link) {
-//       const destination = link.getAttribute("href");
-//       if (destination !== "#") {
-//         window.location.href = destination;
-//       } else {
-//         console.log(
-//           `Navigating to project: ${card.querySelector("h4").innerText}`,
-//         );
-//       }
-//     }
-//   });
-// });
-
 document.querySelectorAll(".faq-item").forEach((item) => {
   item.addEventListener("click", (event) => {
     event.preventDefault();
 
     const answer = item.querySelector(".faq-answer");
     const icon = item.querySelector(".faq-icon");
+    const isCurrentlyHidden = answer.classList.contains("hidden");
 
-    answer.classList.toggle("hidden");
-    icon.classList.toggle("rotate-45");
+    document.querySelectorAll(".faq-item").forEach((otherItem) => {
+      otherItem.querySelector(".faq-answer").classList.add("hidden");
+      otherItem.querySelector(".faq-icon").classList.remove("rotate-45");
+    });
+
+    if (isCurrentlyHidden) {
+      answer.classList.remove("hidden");
+      icon.classList.add("rotate-45");
+    }
   });
 });
